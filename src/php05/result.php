d91c68247d8f011b3d38517f671451042262a919
keke <kekeisozaki13@gmail.com>
2024-05-19 22:03:01 +0900

    ファイルの追加

---
+++ src/php05/result.php
@@ -0,0 +1,57 @@
+
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/sanitize.css">
+    <link rel="stylesheet" href="css/common.css">
+    <link rel="stylesheet" href="css/result.css">
+</head>
+<body>
+    <header class="header">
+        <div class="header__inner">
+            <div class="header__logo-box">
+                <a href="index.php" class="header__logo">World Clock</a>
+            </div>
+            <div class="header__menu">
+                <ul class="header__menu-table">
+                    <li>
+                        <a href="" class="link">概要</a>
+                    </li>
+                    <li>
+                        <a href="" class="link">詳細</a>
+                    </li>
+                    <li>
+                        <a href="" class="link">about us</a>
+                    </li>
+                    <li>
+                        <a href="" class="link">contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </header>
+    <main>
+        <div class="result__content">
+            <div class="result__box">
+                <div class="result__box-img">
+                    <img src="" alt="img/">
+                </div>
+                <div class="result__box-time">
+                    <p class="result__box-text"></p>
+                </div>
+            </div>
+            <div class="result__box">
+                <div class="result__box-img">
+                    <img src="" alt="img/">
+                </div>
+                <div class="result__box-time">
+                    <p class="result__box-text"></p>
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
